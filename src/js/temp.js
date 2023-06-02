976673260b556bd0509b9b480186be35c093756b
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-06-02 11:34:49 +0200

    upd

--- src/js/temp.js
+++ src/js/temp.js
@@ -26,3 +26,30 @@ player.on(
     localStorage.setItem(CURRENT_TIME, time.seconds);
   }, 1000)
 );
+
+// hw 9 - t1
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+startBtnEl.addEventListener('click', startColorChange);
+stopBtnEl.addEventListener('click', stopColorChange);
+let intervalId = null;
+
+function startColorChange() {
+  if (intervalId) {
+    return;
+  }
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
